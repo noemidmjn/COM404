0e3d3f4cc616636c626a81d7d08b945d2d5e293b
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-09-27 12:52:52 +0100

    Create bot.py

---
+++ 1-basics/2-input/5-type-function/bot.py
@@ -0,0 +1,10 @@
+# Begin
+
+first_value = 30.5
+second_value = 5
+
+print(first_value, "is of type", type(first_value))
+print(second_value, "is of type", type(second_value))
+print(first_value, "+", second_value, "=", first_value + second_value)
+
+# End

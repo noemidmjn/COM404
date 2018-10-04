e60e9412c7d0bb70161f0f716971083e3d02130a
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-04 10:32:11 +0100

    Create bot.py
    use of if

---
+++ 1-basics/3-decision/1-if/bot.py
@@ -0,0 +1,14 @@
+#
+print("Please enter a direction from the following:")
+print("...w to move up")
+print("...s to move down")
+print("...a to move left")
+print("...d to move right")
+
+direction = str( input() )
+
+if (direction == "w"):
+  print("I will move up.")
+
+else:
+  print("Error!")

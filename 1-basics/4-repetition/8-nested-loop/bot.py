c228cb5150a6ea05791e8f66b3b82417f6c2205e
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-11 12:30:54 +0100

    Create bot.py

---
+++ 1-basics/4-repetition/8-nested-loop/bot.py
@@ -0,0 +1,12 @@
+#
+print("please enter number of rows")
+rows = int(input())
+
+print("please enter the number of colums")
+columns = int(input())
+
+for count in range(0, rows, 1):
+  for count in range(0, columns, 1):
+    print(":-)", end="")  
+
+  print()

38a58e8d8a0c8ab7e8e9ef56f099cec0e8219765
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-18 09:51:18 +0100

    Create bot.py

---
+++ 1-basics/5-functions/2-leisure/bot.py
@@ -0,0 +1,14 @@
+def activity():
+  print("What should we do?")
+  response = str(input())
+  
+  if (response == "watch movie"):
+    print("watching a movie sounds like fun")
+  elif (response == "play"):
+    print("I have lots of toys to play with")
+  else:
+    print("I am not sure if I will like it but I will give it a try")
+
+activity()
+activity()
+activity()

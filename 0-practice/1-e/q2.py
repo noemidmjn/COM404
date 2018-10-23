c36be9f97ab6e2cfcdd32ed5787650c34cdbe75a
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-23 23:48:18 +0100

    Create q2.py

---
+++ 0-practice/1-e/q2.py
@@ -0,0 +1,7 @@
+# practice paper q2
+print("Have you fear in your heart?")
+response = str(input())
+if response == "yes":
+  print("The force is strong in you. You may be a Jedi apprentice.")
+else:
+  print("The force is strong in you. You may be a Jedi apprentice.")

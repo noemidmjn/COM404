99fee98263b160bc4c158ad4162ea3a888ec9c7e
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-24 01:11:14 +0100

    Rename q3.py to q4.py

---
+++ 0-practice/1-e/q4.py
@@ -0,0 +1,19 @@
+# practice paper q4
+
+def visit(ghost):
+  if (ghost == "Ghost of Christmas Past"):
+    print("Humbug! I care not for these days of past celebration.")
+  elif (ghost == "Ghost of Christmas Present"):
+    print("Humbug! I care not for their suffering.")
+  elif (ghost == "Ghost of Christmas Future"):
+    print("Please no more. I will change my ways. ")
+  else:
+    print("I do not recognise that ghost :(")
+
+
+# The following are calls to the function for testing purposes
+visit("Ghost of Christmas Past") 
+visit("Ghost of Christmas Present") 
+visit("Ghost of Christmas Future")
+
+visit("valamimas")

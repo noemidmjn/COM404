3705248866a8a61b9568fb76de83937edb645190
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-25 09:16:28 +0100

    Update q2.py

--- 0-practice/1-e/q2.py
+++ 0-practice/1-e/q2.py
@@ -2,6 +2,6 @@
 print("Have you fear in your heart?")
 response = str(input())
 if response == "yes":
-  print("The force is strong in you. You may be a Jedi apprentice.")
+  print("Fear is the path to the dark side. You cannot be a Jedi apprentice.")
 else:
   print("The force is strong in you. You may be a Jedi apprentice.")

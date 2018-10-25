ac981897d73ca8d020b5632bc1e70d0c73abb8f4
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-10-25 09:23:06 +0100

    Update q3.py

--- 0-practice/1-e/q3.py
+++ 0-practice/1-e/q3.py
@@ -7,3 +7,15 @@ while (zones > 0):
   zones = zones - 1
   if zones == 0:
     print("Crossed all zones.  Jumanji!")
+
+    
+    
+    
+    print("How many zones must I cross?")
+
+zones = int(input())
+
+print("Crossing zones...")
+
+for count in range (zones, 0, -1):
+  print("...crossing zones", zones)

36a0443e252a895dc42e569da0778ac70b767214
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-11-23 16:13:13 +0000

    Update gui.py

--- 2-guis/4-images/4-hiding-and-showing/gui.py
+++ 2-guis/4-images/4-hiding-and-showing/gui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 class Gui(Tk):
     
@@ -22,6 +23,9 @@ class Gui(Tk):
         self.add_name_entry()
         self.add_passport_entry()
         self.add_nights_entry()
+        self.add_name_image_label()
+        self.add_passport_image_label()
+        self.add_nights_image_label()
 
         
 
@@ -56,46 +60,79 @@ class Gui(Tk):
         self.checkin_button.grid(row=4, column=1, columnspan=3)
         self.checkin_button.configure(font="Arial 20",
                                       text="Check In")
+        self.checkin_button.bind("<ButtonRelease-1>", self.checkin_button_clicked)
 
     def add_name_entry(self):
         self.name_entry = Entry()
         self.name_entry.grid(row=1, column=2, columnspan=2)
+        self.name_entry.bind("<FocusOut>", self.name_entry_focusout)
 
+    def name_entry_focusout(self, event):
+        if (self.name_entry.get() == ""):
+            self.name_image_label.configure(image=self.x_image)
+        else:
+            self.name_image_label.configure(image=self.tick_image)
+            
+        
     def add_passport_entry(self):
         self.passport_entry = Entry()
         self.passport_entry.grid(row=2, column=2, columnspan=2)
+        self.passport_entry.bind("<FocusOut>", self.passport_entry_focusout)
+
+
+    def passport_entry_focusout(self, event):
+        if (self.passport_entry.get() == ""):
+            self.passport_image_label.configure(image=self.x_image)
+        else:
+            self.passport_image_label.configure(image=self.tick_image)
 
     def add_nights_entry(self):
         self.nights_entry = Entry()
         self.nights_entry.grid(row=3, column=2, columnspan=2)
+        self.nights_entry.bind("<FocusOut>", self.nights_entry_focusout)
 
-    def add_image_label(self):
-        self.image_label = Label()
-        self.image_label.grid(row=1, column=4, columnspan=1)
-        self.image_label.configure(image=self.x_image,
+    def nights_entry_focusout(self, event):
+        if (self.nights_entry.get() == ""):
+            self.nights_image_label.configure(image=self.x_image)
+        else:
+            self.nights_image_label.configure(image=self.tick_image)
+        
+
+    def add_name_image_label(self):
+        self.name_image_label = Label()
+        self.name_image_label.grid(row=1, column=4, columnspan=1)
+        self.name_image_label.configure(image=self.x_image,
                                    height=30,
                                    width=30)
 
-    def add_image_label(self):
-        self.image_label = Label()
-        self.image_label.grid(row=2, column=4, columnspan=1)
-        self.image_label.configure(image=self.x_image,
+    def add_passport_image_label(self):
+        self.passport_image_label = Label()
+        self.passport_image_label.grid(row=2, column=4, columnspan=1)
+        self.passport_image_label.configure(image=self.x_image,
                                    height=30,
                                    width=30)
 
-    def add_image_label(self):
-        self.image_label = Label()
-        self.image_label.grid(row=3, column=4, columnspan=1)
-        self.image_label.configure(image=self.x_image,
+    def add_nights_image_label(self):
+        self.nights_image_label = Label()
+        self.nights_image_label.grid(row=3, column=4, columnspan=1)
+        self.nights_image_label.configure(image=self.x_image,
                                    height=30,
                                    width=30)
 
     def checkin_button_clicked(self, event):
-        if there is something in the box:
-            self.image_label.configure(image=self.tick_image)
-
+        if (self.name_entry.get() == "" or
+            self.passport_entry.get() == "" or
+            self.nights_entry.get() == ""):
+            messagebox.showinfo("result", "Checking In Unsuccessful")
         else:
-            self.image_label.configure(image=self.x_image)
+            nights = int(self.nights_entry.get())
+            if nights > 0 and nights <= 365:
+                messagebox.showinfo("result", "Checking In Successful")
+            else:
+                messagebox.showinfo("result", "Number of nights must be between 1 and 365 (inclusive)")
+
+        
+
         
 
 

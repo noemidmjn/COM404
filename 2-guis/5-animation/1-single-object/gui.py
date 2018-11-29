9c66a2d1a65f371fe750a5f05affff8369c66617
noemidmjn <43637224+noemidmjn@users.noreply.github.com>
2018-11-29 10:11:55 +0000

    Update gui.py

--- 2-guis/5-animation/1-single-object/gui.py
+++ 2-guis/5-animation/1-single-object/gui.py
@@ -14,10 +14,10 @@ class AnimatedGui(Tk):
                        width=500)
 
         # set animation attributes
-        self.ball_x_pos = 200
-        self.ball_y_pos = 200
-        self.ball_x_change = 1
-        self.ball_y_change = 1
+        self.ball_x_pos = 250
+        self.ball_y_pos = 80
+        self.ball_x_change = 10
+        self.ball_y_change = 10
 
 
         # add components
@@ -30,10 +30,103 @@ class AnimatedGui(Tk):
 
     # the timer tick function    
     def tick(self):
-        self.ball_x_pos = self.ball_x_pos + 1
-        self.ball_y_pos = self.ball_y_pos + 1
+        self.ball_x_pos = self.ball_x_pos + self.ball_x_change
+        self.ball_y_pos = self.ball_y_pos + self.ball_y_change
         self.ball_image_label.place(x=self.ball_x_pos, 
                                     y=self.ball_y_pos)
+
+
+
+        # check if hit right side
+        if (self.ball_x_pos >= 420):
+            self.ball_x_change = -self.ball_x_change
+
+        # check if hit bottom
+        if (self.ball_y_pos >= 420):
+            self.ball_y_change = -self.ball_y_change
+
+        # check if hit left side
+        if (self.ball_x_pos <= 80):
+            self.ball_x_change = -self.ball_x_change
+
+        # check if hit top
+        if (self.ball_y_pos <= 80):
+            self.ball_y_change = -self.ball_y_change
+        
+
+        self.after(100, self.tick)
+
+
+
+    # the ball image
+    def add_ball_image_label(self):
+        self.ball_image_label = Label()
+        self.ball_image_label.place(x=self.ball_x_pos, 
+                                    y=self.ball_y_pos)
+        self.ball_image_label.configure(image=self.ball_image)
+
+
+    
+# the object
+if __name__ == "__main__":
+    gui = AnimatedGui()
+    gui.mainloop()
+from tkinter import *
+import time
+
+# the class
+class AnimatedGui(Tk):
+    def __init__(self):
+        super().__init__()
+
+        # load resources
+        self.ball_image = PhotoImage(file="ball.png")
+        
+        # set window attributes
+        self.configure(height=500,
+                       width=500)
+
+        # set animation attributes
+        self.ball_x_pos = 250
+        self.ball_y_pos = 80
+        self.ball_x_change = 10
+        self.ball_y_change = 10
+
+
+        # add components
+        self.add_ball_image_label()
+        
+
+        # start the timer
+        self.tick()
+        
+
+    # the timer tick function    
+    def tick(self):
+        self.ball_x_pos = self.ball_x_pos + self.ball_x_change
+        self.ball_y_pos = self.ball_y_pos + self.ball_y_change
+        self.ball_image_label.place(x=self.ball_x_pos, 
+                                    y=self.ball_y_pos)
+
+
+
+        # check if hit right side
+        if (self.ball_x_pos >= 420):
+            self.ball_x_change = -self.ball_x_change
+
+        # check if hit bottom
+        if (self.ball_y_pos >= 420):
+            self.ball_y_change = -self.ball_y_change
+
+        # check if hit left side
+        if (self.ball_x_pos <= 80):
+            self.ball_x_change = -self.ball_x_change
+
+        # check if hit top
+        if (self.ball_y_pos <= 80):
+            self.ball_y_change = -self.ball_y_change
+        
+
         self.after(100, self.tick)
 
 
